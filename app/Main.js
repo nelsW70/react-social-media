9e64f8c6eb5eedffa685189922c886cfc62433d6
Nelson Foltz <nelsfoltz@gmail.com>
2020-08-08 10:17:27 -0400

    add: created Footer component and added temp content

--- app/Main.js
+++ app/Main.js
@@ -2,35 +2,14 @@ import React from 'react'
 import ReactDOM from 'react-dom'
 import Header from './components/Header'
 import HomeGuest from './components/HomeGuest'
+import Footer from './components/Footer'
 
 function ExampleComponent() {
   return (
     <>
       <Header />
       <HomeGuest />
-
-      <footer className="border-top text-center small text-muted py-3">
-        <p>
-          <a href="/" className="mx-1">
-            Home
-          </a>{' '}
-          |
-          <a className="mx-1" href="/about-us">
-            About Us
-          </a>{' '}
-          |
-          <a className="mx-1" href="/terms">
-            Terms
-          </a>
-        </p>
-        <p className="m-0">
-          Copyright &copy; 2020{' '}
-          <a href="/" className="text-muted">
-            ComplexApp
-          </a>
-          . All rights reserved.
-        </p>
-      </footer>
+      <Footer />
     </>
   )
 }
