6bf2aa130a9c272a4d0b00ffa08ff662276d9078
Nelson Foltz <nelsfoltz@gmail.com>
2020-08-09 10:14:03 -0400

    add: starting login functionality

--- app/components/HeaderLoggedOut.js
+++ app/components/HeaderLoggedOut.js
@@ -1,11 +1,33 @@
-import React, { useEffect } from 'react'
+import React, { useEffect, useState } from 'react'
+import Axios from 'axios'
 
 function HeaderLoggedOut() {
+  const [username, setUsername] = useState()
+  const [password, setPassword] = useState()
+
+  async function handleSubmit(e) {
+    e.preventDefault()
+    try {
+      const response = await Axios.post('http://localhost:8080/login', {
+        username,
+        password
+      })
+      if (response.data) {
+        console.log(response.data)
+      } else {
+        console.log('incorrect username / password')
+      }
+    } catch (e) {
+      console.log('There was a problem')
+    }
+  }
+
   return (
-    <form className="mb-0 pt-2 pt-md-0">
+    <form onSubmit={handleSubmit} className="mb-0 pt-2 pt-md-0">
       <div className="row align-items-center">
         <div className="col-md mr-0 pr-md-0 mb-3 mb-md-0">
           <input
+            onChange={e => setUsername(e.target.value)}
             name="username"
             className="form-control form-control-sm input-dark"
             type="text"
@@ -15,6 +37,7 @@ function HeaderLoggedOut() {
         </div>
         <div className="col-md mr-0 pr-md-0 mb-3 mb-md-0">
           <input
+            onChange={e => setPassword(e.target.value)}
             name="password"
             className="form-control form-control-sm input-dark"
             type="password"
