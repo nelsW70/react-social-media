9ee76982d6f31476058f2438fced24f62edb12c9
Nelson Foltz <nelsfoltz@gmail.com>
2020-08-11 08:51:26 -0400

    add: lifted loggedIn state to parent

--- app/components/HeaderLoggedIn.js
+++ app/components/HeaderLoggedIn.js
@@ -20,7 +20,7 @@ function HeaderLoggedIn(props) {
       <a href="#" className="mr-2">
         <img
           className="small-header-avatar"
-          src="https://gravatar.com/avatar/b9408a09298632b5151200f3449434ef?s=128"
+          src={localStorage.getItem('complexappAvatar')}
         />
       </a>
       <a className="btn btn-sm btn-success mr-2" href="/create-post">
