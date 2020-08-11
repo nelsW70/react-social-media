3933b945eabdc693945fc11855a8a573169f6c9b
Nelson Foltz <nelsfoltz@gmail.com>
2020-08-11 12:45:04 -0400

    add: dynamic route and navigate to it  for new post

--- app/components/CreatePost.js
+++ app/components/CreatePost.js
@@ -1,22 +1,25 @@
 import React, { useState } from 'react'
 import Page from './Page'
 import Axios from 'axios'
+import { withRouter } from 'react-router-dom'
 
-function CreatePost() {
+function CreatePost(props) {
   const [title, setTitle] = useState()
   const [body, setBody] = useState()
 
   async function handleSubmit(e) {
     e.preventDefault()
     try {
-      await Axios.post('create-post', {
+      const response = await Axios.post('create-post', {
         title,
         body,
         token: localStorage.getItem('complexappToken')
       })
-      console.log('new post was created')
+      // Redirect to new post url
+      props.history.push(`/post/${response.data}`)
+      console.log('New post was created.')
     } catch (e) {
-      console.log('There was a problem')
+      console.log('There was a problem.')
     }
   }
 
@@ -58,4 +61,4 @@ function CreatePost() {
   )
 }
 
-export default CreatePost
+export default withRouter(CreatePost)
