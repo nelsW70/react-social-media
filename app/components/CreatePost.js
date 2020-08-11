25124e4ac5d23a4d12cb9cee25e5e3fc407cc784
Nelson Foltz <nelsfoltz@gmail.com>
2020-08-11 10:14:33 -0400

    add: state for CreatePost inputs and post method

--- app/components/CreatePost.js
+++ app/components/CreatePost.js
@@ -1,14 +1,17 @@
-import React from 'react'
+import React, { useState } from 'react'
 import Page from './Page'
 import Axios from 'axios'
 
 function CreatePost() {
+  const [title, setTitle] = useState()
+  const [body, setBody] = useState()
+
   async function handleSubmit(e) {
     e.preventDefault()
     try {
       await Axios.post('create-post', {
-        title: 'Test Tile',
-        body: 'test content here',
+        title,
+        body,
         token: localStorage.getItem('complexappToken')
       })
       console.log('new post was created')
@@ -25,6 +28,7 @@ function CreatePost() {
             <small>Title</small>
           </label>
           <input
+            onChange={e => setTitle(e.target.value)}
             autoFocus
             name="title"
             id="post-title"
@@ -40,6 +44,7 @@ function CreatePost() {
             <small>Body Content</small>
           </label>
           <textarea
+            onChange={e => setBody(e.target.value)}
             name="body"
             id="post-body"
             className="body-content tall-textarea form-control"
