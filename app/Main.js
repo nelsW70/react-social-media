3bf9184de73e5b475808c96d5c4968aef424e7c0
Nelson Foltz <nelsfoltz@gmail.com>
2020-08-12 13:00:52 -0400

    add: implementing useEffect for logged in status in Main.js

--- app/Main.js
+++ app/Main.js
@@ -1,4 +1,4 @@
-import React, { useReducer } from 'react'
+import React, { useReducer, useEffect } from 'react'
 import ReactDOM from 'react-dom'
 import { useImmerReducer } from 'use-immer'
 import { BrowserRouter, Switch, Route } from 'react-router-dom'
@@ -21,13 +21,19 @@ import FlashMessages from './components/FlashMessages'
 function Main() {
   const initialState = {
     loggedIn: Boolean(localStorage.getItem('complexappToken')),
-    flashMessages: []
+    flashMessages: [],
+    user: {
+      token: localStorage.getItem('complexappToken'),
+      username: localStorage.getItem('complexappUsername'),
+      avatar: localStorage.getItem('complexappAvatar')
+    }
   }
 
   function ourReducer(draft, action) {
     switch (action.type) {
       case 'login':
         draft.loggedIn = true
+        draft.user = action.data
         return
       case 'logout':
         draft.loggedIn = false
@@ -39,6 +45,18 @@ function Main() {
   }
 
   const [state, dispatch] = useImmerReducer(ourReducer, initialState)
+
+  useEffect(() => {
+    if (state.loggedIn) {
+      localStorage.setItem('complexappToken', state.user.token)
+      localStorage.setItem('complexappUsername', state.user.username)
+      localStorage.setItem('complexappAvatar', state.user.avatar)
+    } else {
+      localStorage.removeItem('complexappToken')
+      localStorage.removeItem('complexappUsername')
+      localStorage.removeItem('complexappAvatar')
+    }
+  }, [state.loggedIn])
 
   return (
     <StateContext.Provider value={state}>
