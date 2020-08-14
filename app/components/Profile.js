cf7f8c5104c009aead7a839eb85d515e54e3e2f9
Nelson Foltz <nelsfoltz@gmail.com>
2020-08-14 08:47:33 -0400

    add: 40. profile screen

--- app/components/Profile.js
+++ app/components/Profile.js
@@ -1,15 +1,38 @@
-import React from 'react'
+import React, { useEffect, useContext, useState } from 'react'
 import Page from './Page'
+import { useParams } from 'react-router-dom'
+import Axios from 'axios'
+import StateContext from '../StateContext'
 
 function Profile() {
+  const { username } = useParams()
+  const appState = useContext(StateContext)
+  const [profileData, setProfileData] = useState({
+    profileUsername: '...',
+    profileAvatar: 'https://gravatar.com/avatar/placeholder?s=128',
+    isFollowing: false,
+    counts: { postCount: '', followerCount: '', followingCount: '' }
+  })
+
+  useEffect(() => {
+    async function fetchData() {
+      try {
+        const response = await Axios.post(`/profile/${username}`, {
+          token: appState.user.token
+        })
+        setProfileData(response.data)
+      } catch (e) {
+        console.log('There was a problem.')
+      }
+    }
+    fetchData()
+  }, [])
+
   return (
     <Page title="Profile Screen">
       <h2>
-        <img
-          className="avatar-small"
-          src="https://gravatar.com/avatar/b9408a09298632b5151200f3449434ef?s=128"
-        />{' '}
-        brad
+        <img className="avatar-small" src={profileData.profileAvatar} />
+        {profileData.profileUsername}
         <button className="btn btn-primary btn-sm ml-2">
           Follow <i className="fas fa-user-plus"></i>
         </button>
@@ -17,13 +40,13 @@ function Profile() {
 
       <div className="profile-nav nav nav-tabs pt-2 mb-4">
         <a href="#" className="active nav-item nav-link">
-          Posts: 3
+          Posts: {profileData.counts.postCount}
         </a>
         <a href="#" className="nav-item nav-link">
-          Followers: 101
+          Followers: {profileData.counts.postCount}
         </a>
         <a href="#" className="nav-item nav-link">
-          Following: 40
+          Following: {profileData.counts.followingCount}
         </a>
       </div>
 
